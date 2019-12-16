8528e20a871d68d7d77f509f5f89f96d2bdcc4d5
tobihoff <lebenskraft.th@gmail.com>
2019-12-16 19:41:23 +0100

    Add theming, storybooks add-on and GlobalStyles to storybook
    closes #2

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,4 +1,19 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { ThemeProvider } from 'emotion-theming';
+import GlobalStyles from '../src/components/GlobalStyles';
+import theme from '../src/components/themes/theme';
+import { withKnobs } from '@storybook/addon-knobs';
+
+// add GlobalStyle for every story
+const GlobalStyleDecorator = storyFn => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    {storyFn()}
+  </ThemeProvider>
+);
+addDecorator(GlobalStyleDecorator);
+addDecorator(withKnobs);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.js$/), module);
---
+++ client/src/components/themes/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  color: {
+    primary: '#f7f7f7',
+    secondary: '#353535',
+    third: '#03F293'
+  }
+};
+
+export default theme;
