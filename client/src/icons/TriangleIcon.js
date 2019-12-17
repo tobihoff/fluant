827c0901b388d5878cf080a91bc99308854f9ecd
tobihoff <lebenskraft.th@gmail.com>
2019-12-17 19:12:36 +0100

    Change icons to function for better functionality

---
+++ client/src/icons/TriangleIcon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TriangleIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000000"
+      viewBox="0 0 24 24"
+      width="96px"
+      height="96px"
+    >
+      {' '}
+      <path d="M 1 4 L 12 22.300781 L 23 4 L 1 4 z" />
+    </svg>
+  );
+}
