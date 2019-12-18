5f6b91a5541de91f315a9cfe85ced6b172573546
tobihoff <lebenskraft.th@gmail.com>
2019-12-18 17:28:11 +0100

    Add card to component and storybook

---
+++ client/src/components/Cards.js
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  max-width: 240px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+  grid-gap: 5px;
+  height: 100%;
+  justify-content: center;
+`;
+
+export const Card = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  border-radius: 10px;
+  margin-top: 10px;
+  background-color: white;
+  border: none;
+`;
