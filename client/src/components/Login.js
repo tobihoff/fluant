de0a046118ae8aa88c93a0974f0bb4a54da827d6
tobihoff <lebenskraft.th@gmail.com>
2019-12-18 14:13:11 +0100

    Add login to components and storybook

---
+++ client/src/components/Login.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const LoginName = styled.input`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  text-align: center;
+  font-size: 12px;
+  color: ${props => props.theme.secondary};
+  border: 1px solid ${props => props.theme.third};
+  background-image: url('../icons/profile.svg') no-repeat;
+  outline: none;
+  width: 200px;
+  height: 30px;
+  margin: 5px;
+`;
+
+export default LoginName;
