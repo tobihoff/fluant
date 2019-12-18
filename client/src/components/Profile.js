6531f3783cbdd94829873429e56e23d6c33a49d2
tobihoff <lebenskraft.th@gmail.com>
2019-12-18 14:09:36 +0100

    Add Profile to components and storybook

---
+++ client/src/components/Profile.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Profile = styled.img`
+  width: 100%;
+  height: 100%;
+  max-width: 100px;
+  max-height: 100px;
+  object-fit: cover;
+  border: none;
+  border-radius: 50%;
+`;
+
+export default Profile;
