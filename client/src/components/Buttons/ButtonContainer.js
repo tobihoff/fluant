facd4e3b4cbe545e441a29e773306ca86e0c7e6c
tobihoff <lebenskraft.th@gmail.com>
2019-12-22 11:05:07 +0100

    Add Button to ProfileFriends component

---
+++ client/src/components/Buttons/ButtonContainer.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 240px;
+  margin-bottom: 10px;
+`;
--- client/src/components/Profile/ProfileFriends.js
+++ client/src/components/Profile/ProfileFriends.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { CardContainer } from '../Cards/Cards';
 import ProfileList from '../ProfileList';
 import { FriendsProfile, ProfileInfo, ProfileScore } from '../Profile';
+import Button from '../Buttons/Buttons';
+import { ButtonContainer } from '../Buttons/ButtonContainer';
 
 export default function ProfileFriends() {
   return (
     <CardContainer>
+      <ButtonContainer>
+        <Button>Add Friends</Button>
+      </ButtonContainer>
       <ProfileList>
         <FriendsProfile />
         <ProfileInfo>Franzi</ProfileInfo>
