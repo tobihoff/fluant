4f087b739f4dd7034c594d95a36cfaf36f8684d5
tobihoff <lebenskraft.th@gmail.com>
2019-12-22 10:46:56 +0100

    Add ProfileFriends to components

--- client/src/components/Profile/ProfileFriends.js
+++ client/src/components/Profile/ProfileFriends.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { CardContainer } from '../Cards/Cards';
+import ProfileList from '../ProfileList';
+import { FriendsProfile, ProfileInfo, ProfileScore } from '../Profile';
 
 export default function ProfileFriends() {
-  return <h1>Hallo</h1>;
+  return (
+    <CardContainer>
+      <ProfileList>
+        <FriendsProfile />
+        <ProfileInfo>Franzi</ProfileInfo>
+        <ProfileScore>300XP</ProfileScore>
+      </ProfileList>
+    </CardContainer>
+  );
 }
