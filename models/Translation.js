40a7d677f3df9eb7f7b418dd46eeb85ceb912e19
tobihoff <lebenskraft.th@gmail.com>
2020-01-12 10:07:59 +0100

    Refactor backend components regarding translator

--- models/Translation.js
+++ models/Translation.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
 const TranslationSchema = mongoose.Schema({
-  german: {
-    type: String,
-    required: true
-  },
-  english: {
-    type: String,
-    required: true
-  }
+  voca: [
+    {
+      german: {
+        type: String,
+        required: true
+      }
+    },
+    {
+      english: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model('Translation', TranslationSchema);
