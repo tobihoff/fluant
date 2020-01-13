c104a939d84dce3729c4e0a0055ab598dbe8a999
tobihoff <lebenskraft.th@gmail.com>
2020-01-13 11:33:23 +0100

    Add process.env for Heroku deploymnet

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
-const jwt = require("jsonwebtoken");
-const config = require("config");
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 module.exports = function(req, res, next) {
   //Get token from header
-  const token = req.header("x-auth-token");
+  const token = req.header('x-auth-token');
 
   //Check if not token
   if (!token) {
-    return res.status(401).json({ msg: "Not token, authorization denied" });
+    return res.status(401).json({ msg: 'Not token, authorization denied' });
   }
 
   //Verify token
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || config.get('jwtSecret'));
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
+    res.status(401).json({ msg: 'Token is not valid' });
   }
 };
